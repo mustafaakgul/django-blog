acce0203c5d5da2c83dfffe910a4a6595eb860b8
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-02 17:38:19 +0300

    upd contact model

--- contacts/models.py
+++ contacts/models.py
@@ -6,11 +6,11 @@ class Contact(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=30)
     message = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'contact'
-        verbose_name = 'Contact'
-        verbose_name_plural = 'Contacts'
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.email
