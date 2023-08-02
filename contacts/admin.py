1055dd5ace7d1a053014ee3fe6f908ef96631fe8
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-08-02 17:39:36 +0300

    upd contact admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Contact
 
 
-admin.site.register(Contact)
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Contact._meta.fields]
+    list_display_links = ["email", "phone", "created_at"]
+    search_fields = ["email", "phone"]
+    list_filter = ["email", "created_at"]
+
+    class Meta:
+        model = Contact
